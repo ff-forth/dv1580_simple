70339ae754dde162f141da680bc5176d947ce581
ff-forth <130032877+ff-forth@users.noreply.github.com>
2024-09-19 12:33:50 +0200

    Update mult.c

--- mult.c
+++ mult.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //atof <-> ASCII to fload
+
+void mult(float a, float b){
+    float result = a*b;
+    printf("%.4f\n" ,result);
+}
 
 int main(int argc, char *argv[]){
+    // convert arguments to float
+    float a = atof(argv[1]);
+    float b = atof(argv[2]);
+    // call mult function
+    mult(a,b);
+    return 0;
 
-  return 0;
 }
+
+// gcc -o mult mult.c && ./mult 3.14 3.14
