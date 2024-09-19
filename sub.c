60ad65a18330555334f3093c5580a46fccc5caf8
ff-forth <130032877+ff-forth@users.noreply.github.com>
2024-09-19 12:33:26 +0200

    Update sub.c

--- sub.c
+++ sub.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-
-  return 0;
+void sub(int a, int b){
+    int result = a - b;
+    printf("%d\n" ,result);
 }
+
+int main(int argc, char *argv[]){
+    // convert arguments to integer
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    // call sub function
+    sub(a,b);
+    return 0;
+}
+
+// gcc -o sub sub.c && ./sub 1 2
+// gcc -o sub sub.c && ./sub 2 1
