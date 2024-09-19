0ef8f078c8a8cdcd2b64149cc1561d812e6d5237
ff-forth <130032877+ff-forth@users.noreply.github.com>
2024-09-19 12:32:03 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> 
+
+void add(int a, int b){
+    int result = a + b;
+    printf("%d\n" ,result);
+}
 
 int main(int argc, char *argv[]){
-
-  return 0;
+    // convert arguments to floategers
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    // call add function
+    add(a,b);
+    return 0;
 }
+
+// gcc -o add add.c && ./add 1 2
